1a42bf8536c206bb1ef7f4902a7553315c407af6
Matteo Susca <matteo.susca@studio.unibo.it>
2025-03-20 13:55:36 +0100

    FPS From frametime

--- src/main/java/pcd/ass01/BoidsSimulator.java
+++ src/main/java/pcd/ass01/BoidsSimulator.java
@@ -7,7 +7,7 @@ public class BoidsSimulator {
     private BoidsModel model;
     private Optional<BoidsView> view;
     
-    private static final int FRAMERATE = 60;
+    private static final int FRAMERATE = 3000;
     private int framerate;
     
     public BoidsSimulator(BoidsModel model) {
@@ -23,43 +23,24 @@ public class BoidsSimulator {
         int nBoids = model.getThreads().stream().mapToInt(t -> t.getBoids().size()).sum();
         System.out.println("Number of boids: " + nBoids);
         model.getThreads().forEach(Thread::start);
-    	while (true) {
-            var t0 = System.currentTimeMillis();
-    		/* 
-    		 * Improved correctness: first update velocities...
-    		 */
-//    		for (Boid boid : boids) {
-//                boid.updateVelocity(model);
-//            }
 
-    		/* 
-    		 * ..then update positions
-    		 */
-//    		for (Boid boid : boids) {
-//                boid.updatePos(model);
-//            }
+        long lastTime = System.nanoTime();
+
+        while (true) {
+            long now = System.nanoTime();
             try {
                 this.model.getBarrier().await();
             } catch (Exception e) {
                 e.printStackTrace();
             }
 
-    		if (view.isPresent()) {
-            	view.get().update(framerate);
-            	var t1 = System.currentTimeMillis();
-                var dtElapsed = t1 - t0;
-                var framratePeriod = 1000/FRAMERATE;
-                
-                if (dtElapsed < framratePeriod) {		
-                	try {
-                		Thread.sleep(framratePeriod - dtElapsed);
-                	} catch (Exception ex) {}
-                	framerate = FRAMERATE;
-                } else {
-                	framerate = (int) (1000/dtElapsed);
-                }
-    		}
-            
-    	}
+            //calculate framerate given frametime
+            framerate = (int) (1_000_000_000 / (now - lastTime));
+
+            if (view.isPresent()) {
+                view.get().update(framerate);
+            }
+            lastTime = now;
+        }
     }
 }

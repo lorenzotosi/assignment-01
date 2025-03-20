0185fa697761fe1b9d2896d83b3deacba83d820e
Matteo Susca <matteo.susca@studio.unibo.it>
2025-03-20 11:51:07 +0100

    oopsie

---
+++ src/main/java/worker/MultiWorker.java
@@ -0,0 +1,53 @@
+package worker;
+
+import pcd.ass01.Boid;
+import pcd.ass01.BoidsModel;
+
+import java.util.List;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class MultiWorker extends Thread {
+
+    private final List<Boid> boids;
+    private final BoidsModel boidsModel;
+    private final CyclicBarrier barrier;
+
+    public MultiWorker(List<Boid> boids, BoidsModel boidsModel, CyclicBarrier barrier) {
+        this.boids = boids;
+        this.boidsModel = boidsModel;
+        this.barrier = barrier;
+    }
+
+    public List<Boid> getBoids() {
+        return boids;
+    }
+
+    public void run() {
+        while (true) {
+            try {
+                boids.forEach(boid -> {
+                    //System.out.println("before");
+                    boid.calculateVelocity(boidsModel);
+                });
+                barrier.await();
+                boids.forEach(boid -> {
+                    //System.out.println("before");
+                    boid.updateVelocity(boidsModel);
+                });
+                boids.forEach(boid -> {
+                    //System.out.println("before");
+                    boid.updatePos(boidsModel);
+                });
+                barrier.await();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                break;
+            } catch (BrokenBarrierException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
+
+}

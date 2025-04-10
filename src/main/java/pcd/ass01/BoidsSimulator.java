2bba48a6848a00f55ca50a1a90b52b77fe4d7e21
Andrea Zavatta <zavattaandrea@gmail.com>
2025-04-10 11:58:02 +0200

    print performance ricci

--- src/main/java/pcd/ass01/BoidsSimulator.java
+++ src/main/java/pcd/ass01/BoidsSimulator.java
@@ -6,60 +6,69 @@ public class BoidsSimulator {
 
     private BoidsModel model;
     private Optional<BoidsView> view;
-    
+
     private static final int FRAMERATE = 25;
     private int framerate;
-    
+
+    // Variabili per la misurazione delle performance
+    private long totalTime = 0;
+    private int count = 0;
+    private int blockCounter = 0;
+
     public BoidsSimulator(BoidsModel model) {
         this.model = model;
         view = Optional.empty();
     }
 
     public void attachView(BoidsView view) {
-    	this.view = Optional.of(view);
+        this.view = Optional.of(view);
     }
-      
+
     public void runSimulation() {
-    	while (true) {
+        while (true) {
+
             var t0 = System.currentTimeMillis();
-    		var boids = model.getBoids();
-    		/*
-    		for (Boid boid : boids) {
-                boid.update(model);
-            }
-            */
-    		
-    		/* 
-    		 * Improved correctness: first update velocities...
-    		 */
-    		for (Boid boid : boids) {
+            var boids = model.getBoids();
+
+            // Fase 1: aggiornamento velocità
+            for (Boid boid : boids) {
                 boid.updateVelocity(model);
             }
 
-    		/* 
-    		 * ..then update positions
-    		 */
-    		for (Boid boid : boids) {
+            // Fase 2: aggiornamento posizione
+            for (Boid boid : boids) {
                 boid.updatePos(model);
             }
 
-            
-    		if (view.isPresent()) {
-            	view.get().update(framerate);
-            	var t1 = System.currentTimeMillis();
+            // Aggiornamento della view e gestione framerate
+            if (view.isPresent()) {
+                view.get().update(framerate);
+                var t1 = System.currentTimeMillis();
                 var dtElapsed = t1 - t0;
-                var framratePeriod = 1000/FRAMERATE;
-                
-                if (dtElapsed < framratePeriod) {		
-                	try {
-                		Thread.sleep(framratePeriod - dtElapsed);
-                	} catch (Exception ex) {}
-                	framerate = FRAMERATE;
-                } else {
-                	framerate = (int) (1000/dtElapsed);
+
+                // Logging performance
+                totalTime += dtElapsed;
+                count++;
+                if (count == 100) {
+                    long avg = totalTime / count;
+                    System.out.println("BoidsSimulator - Block " + blockCounter + " - Avg time per frame: " + avg + " ms");
+                    blockCounter++;
+                    totalTime = 0;
+                    count = 0;
                 }
-    		}
-            
-    	}
+
+                var framratePeriod = 1000 / FRAMERATE;
+                if (dtElapsed < framratePeriod) {
+                    try {
+                        Thread.sleep(framratePeriod - dtElapsed);
+                    } catch (Exception ex) {
+                        // Ignora
+                    }
+                    framerate = FRAMERATE;
+                } else {
+                    framerate = (int) (1000 / dtElapsed);
+                }
+            }
+        }
     }
 }
